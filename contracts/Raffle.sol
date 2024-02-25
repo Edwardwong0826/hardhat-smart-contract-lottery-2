5a54eb5c6f8463c66289636e74acf7a6705fdbea
Edwardwong0826 <edwardwong_94@outlook.com>
2024-02-25 17:13:52 +0800

    First commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,17 @@
+// Raffle
+
+// Enter the lottery (paying some amount)
+
+// Pick a random winner (verifiably random)
+
+// Winner to be selected every X minutes -> completly automated
+
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keeper)
+
+// SPDX-License-Identifier: mit
+
+pragma solidity ^0.8.19;
+
+contract Raffle{
+    
+}
